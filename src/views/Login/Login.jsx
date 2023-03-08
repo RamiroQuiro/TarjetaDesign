fe2b7fef6ebbaff2c1be28f7409e886b3366c021
ramiroquiro <rama.exe.13@gmail.com>
2023-03-08 20:57:06 -0300

    creando tarjeta de login

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div className='w-scree h-screen bg-red-200 flex items-center justify-center overflow-hidden'>
-     <section className='bg-gradient-to-t to-gray-50 from-slate-400 h-full w-6 absolute'>
+    <div className='w-screen h-screen bg-red-500 flex items-center justify-center overflow-hidden absolute'>
+     <section className="bg-white h-[400px] w-[400px]  relative flex items-center justify-center before:content-[' '] before:absolute before:top-0 before:left-0 before:w-6 before:h-full before:bg-gradient-to-t before:to-gray-50 before:from-slate-400 before:backdrop-blur-sm before:z-10
+     after:content-[' '] after:absolute after:top-0 after:right-0 after:w-6 after:h-full after:bg-gradient-to-t after:to-gray-50 after:from-slate-400 after:backdrop-blur-sm after:z-10
+     ">
       <div></div>
       <div> </div>
       </section> 
