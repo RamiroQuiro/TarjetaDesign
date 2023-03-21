5f612bf91fef246a3dc911cf4b87e52e6ee14e92
ramiroquiro <rama.exe.13@gmail.com>
2023-03-21 15:31:32 -0300

    creando ubicaion

--- src/views/Secciones/Outlet/Ubicacion.jsx
+++ src/views/Secciones/Outlet/Ubicacion.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+import React from "react";
 
 export default function Ubicacion() {
   return (
-    <div className="flex  flex-col w-full mx-auto py-10 px-5">Ubicacion</div>
-  )
+    <div className="flex  flex-col w-full mx-auto py-10 px-5">
+      <section className="text-gray-700 rounded  relative">
+        <div className="absolute h- inset-0 rounded bg-gray-300">
+          <iframe
+            width="100%"
+            height="100%"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+            title="map"
+          className="rounded "
+           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d112936.55017955673!2d-64.33708854505777!3d-27.801545286398238!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x943b526adf33f96b%3A0x9a9d18ed7764c8d6!2sSantiago%20del%20Estero!5e0!3m2!1ses!2sar!4v1679417953350!5m2!1ses!2sar"
+            // src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
+          ></iframe>
+        </div>
+        <div className=" px-5 py-24 mx-auto flex">
+          <div className="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+            <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
+              Feedback
+            </h2>
+            <p className="leading-relaxed mb-5 text-gray-600">
+              Post-ironic portland shabby chic echo park, banjo fashion axe
+            </p>
+            <div className="relative mb-4">
+              <label for="email" className="leading-7 text-sm text-gray-600">
+                Email
+              </label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              />
+            </div>
+            <div className="relative mb-4">
+              <label for="message" className="leading-7 text-sm text-gray-600">
+                Message
+              </label>
+              <textarea
+                id="message"
+                name="message"
+                className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              ></textarea>
+            </div>
+            <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+              Button
+            </button>
+            <p className="text-xs text-gray-500 mt-3">
+              Chicharrones blog helvetica normcore iceland tousled brook viral
+              artisan.
+            </p>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
