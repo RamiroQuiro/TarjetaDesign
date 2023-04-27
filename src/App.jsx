1b0bf8c23c3178a5dcfda32fef07f69ebad300da
ramiroquiro <rama.exe.13@gmail.com>
2023-04-27 15:37:25 -0300

    cambiando sit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, useNavigate } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import Home from "../src/views/Tarjeta1/Home";
 import Login from "../src/views/Login/Login";
